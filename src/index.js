06c3a33f1fc2e98a8651e3435c7562437fda9ef8
Buddo-ID <cher_new@mail.ru>
2021-08-21 15:10:43 +0300

    task decide

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
-}
+    let mod = Math.abs(n);
+    let str = mod.toString();
+    let result = ''; 
+    
+    for (let i = 0; i < str.length; i = i + 1) {
+        result = `${str[i]}${result}`;
+    }
+
+    return Number(result);
+
+};
